39b022b5c7544688244f22c62d13046e79c26198
Veeti Tiirola <veeti.tiirola@hotmail.com>
2025-01-13 15:57:19 +0200

    Tavallisen mergen tekeminen

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,2 @@
-print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello from feature")
-print("Hello from release")
-=======
->>>>>>> intermediate/master
-=======
 print("Hello once again")
->>>>>>> intermediate/feature/add-logging
+
