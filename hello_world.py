89ede05fd1bf7691db60bef2b4e52f9b3eb44966
Veeti Tiirola <veeti.tiirola@hotmail.com>
2025-01-13 15:19:00 +0200

    Lisätty tehtäväannon viesti Hello from feature tiedostoon

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello World!") 
-=======
+
 print("Hello world!")
->>>>>>> upstream/master
-=======
-print("Hello world!")
->>>>>>> upstream/master
+print("Hello from feature")
